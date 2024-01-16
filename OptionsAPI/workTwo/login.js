e10992e9c286b8ae53dc8611769ad978fbffc174
yuhsiang777 <da0226894713@gmail.com>
2024-01-16 16:21:45 +0800

    撰寫"Login"部分

--- OptionsAPI/workTwo/login.js
+++ OptionsAPI/workTwo/login.js
@@ -1,12 +1,25 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+const url = "https://vue3-course-api.hexschool.io/v2";
+const path = "";
+
 createApp({
   data() {
     return {
-      
-    }
+      user: {
+        username: "",
+        password: "",
+      },
+    };
   }, 
   methods: {
-    
+    login(){
+      axios
+       .path
+       .then
+       .catch(){
+        
+       }
+    }
   },
 }).mount("#app");
